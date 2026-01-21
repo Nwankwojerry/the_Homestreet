4e701a1d66757be6ead8c2d2ef264bea93a46abf
Nwankwojerry <jerryboystar69@gmail.com>
2026-01-21 04:48:38 +0000

    updates

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// js/main.js
+console.log("JavaScript is successfully linked!");
+
+// Basic test function
+function testConnection() {
+    alert("JS Link Verified");
+}
+
+// Initialize
+window.onload = function() {
+    console.log("Page fully loaded");
+};
